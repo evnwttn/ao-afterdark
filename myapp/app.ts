0111578f8f1e3540b458e96701c3133403878846
evnwttn <evnwttn@gmail.com>
2022-08-10 21:52:28 -0230

    js -> ts

---
+++ myapp/app.ts
@@ -0,0 +1,35 @@
+import * as express from 'express';
+const app = express.default();
+const port = 3005;
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
+
+
+
+///
+
+// Importing the express package
+
+// Import middleware
+// import { logger, corsHandler } from './middleware';
+
+// Init express app
+
+// Register middlewares
+// app.use(logger);
+// app.use(corsHandler);
+
+// app.post("/contact", (req, res) => {
+// 	console.log('here')
+// 	console.log(JSON.stringify(req.body, null, 4))
+
+// 	res
+// 	.header('Access-Control-Allow-Origin', '*')	 			
+// 	.status(200)	
+// 	.json({ data: 'this should work' });
+// });
+
+// Start our server and listen on port 3005
+// app.listen(port, () => console.log("server listening @ 3005"));
