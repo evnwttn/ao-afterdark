2e74b61dd5b297eb3ff2eeb0f33eec023d3904a0
evnwttn <evnwttn@gmail.com>
2022-08-12 16:28:48 -0230

    body parser

--- myapp/build/middleware/logger.js
+++ myapp/build/middleware/logger.js
@@ -4,6 +4,7 @@ exports.logger = void 0;
 //@ts-ignore
 function logger(req, res, next) {
     console.log(`[REQUEST] ${req.path}`);
+    console.log(req.body);
     next();
 }
 exports.logger = logger;
