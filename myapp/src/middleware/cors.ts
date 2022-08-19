7747c36be55a917e19a97155168e1dda4e313d56
evnwttn <evnwttn@gmail.com>
2022-08-19 14:43:20 -0230

    yeye

--- myapp/src/middleware/cors.ts
+++ myapp/src/middleware/cors.ts
@@ -1,6 +1,5 @@
 //@ts-ignore
 export function cors(req, res, next) {
-  console.log("cors middleware");
   res.set({
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "*",
