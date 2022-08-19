293789bc3824a0e90375490368268f3699888541
evnwttn <evnwttn@gmail.com>
2022-08-19 15:56:53 -0230

    dotenv

--- myapp/build/handlers/contacts.js
+++ myapp/build/handlers/contacts.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -33,20 +10,26 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.contactsHandler = void 0;
-const fs = __importStar(require("fs/promises"));
+// import * as fs from 'fs/promises';
+// import emailjs from '@emailjs/browser';
+require('dotenv').config();
 function contactsHandler(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
-        console.log(JSON.stringify(req.body, null, 4));
-        console.log(req.body.name);
-        try {
-            yield fs.writeFile('test.json', req.body);
-            res
-                .status(200)
-                .json({ message: `yo` });
-        }
-        catch (error) {
-            res.sendStatus(500);
-        }
+        console.log(process.env.EMAILJS_SERVICE_ID);
     });
 }
 exports.contactsHandler = contactsHandler;
+// export async function contactsHandler(req: Request, res: Response) {
+//     console.log(JSON.stringify(req.body, null, 4));
+//     // console.log(req.body.name)
+//     // console.log(req.body.email)
+//     // console.log(req.body.message)
+//     try {
+//       await fs.writeFile('test.json', req.body);
+//       res
+//         .status(200)
+//         .json({ message: `yo` });
+//     } catch (error) {
+//       res.sendStatus(500)
+//     }
+// }
--- myapp/src/handlers/contacts.ts
+++ myapp/src/handlers/contacts.ts
@@ -1,18 +1,24 @@
 import { Request, Response } from 'express';
-import * as fs from 'fs/promises';
-import emailjs from '@emailjs/browser';
+// import * as fs from 'fs/promises';
+// import emailjs from '@emailjs/browser';
+
+require('dotenv').config()
 
 export async function contactsHandler(req: Request, res: Response) {
-    console.log(JSON.stringify(req.body, null, 4));
-    // console.log(req.body.name)
-    // console.log(req.body.email)
-    // console.log(req.body.message)
-    try {
-      await fs.writeFile('test.json', req.body);
-      res
-        .status(200)
-        .json({ message: `yo` });
-    } catch (error) {
-      res.sendStatus(500)
-    }
+console.log(process.env.EMAILJS_SERVICE_ID)
 }
+
+// export async function contactsHandler(req: Request, res: Response) {
+//     console.log(JSON.stringify(req.body, null, 4));
+//     // console.log(req.body.name)
+//     // console.log(req.body.email)
+//     // console.log(req.body.message)
+//     try {
+//       await fs.writeFile('test.json', req.body);
+//       res
+//         .status(200)
+//         .json({ message: `yo` });
+//     } catch (error) {
+//       res.sendStatus(500)
+//     }
+// }
