119c1cb4b8207c13827ac62338679485d94399c5
evnwttn <evnwttn@gmail.com>
2022-08-20 10:24:55 -0230

    Update contacts.ts

--- myapp/src/handlers/contacts.ts
+++ myapp/src/handlers/contacts.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-// import * as fs from 'fs/promises';
 import * as emailjs from '@emailjs/browser';
 
 export async function contactsHandler(req: Request, res: Response) {
@@ -21,26 +20,8 @@ export async function contactsHandler(req: Request, res: Response) {
         });
     res
       .status(200)
-      .json({ message: `yo` });
+      .json({ message: `email from ${req.body.name} sent` });
   } catch (error) {
     res.sendStatus(500)
   }
-
-  console.log(process.env.EMAILJS_USER_ID)
-
 }
-
-// export async function contactsHandler(req: Request, res: Response) {
-//     console.log(JSON.stringify(req.body, null, 4));
-//     // console.log(req.body.name)
-//     // console.log(req.body.email)
-//     // console.log(req.body.message)
-//     try {
-//       await fs.writeFile('test.json', req.body);
-//       res
-//         .status(200)
-//         .json({ message: `yo` });
-//     } catch (error) {
-//       res.sendStatus(500)
-//     }
-// }
