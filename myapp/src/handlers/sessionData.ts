fb30cb3f2056bfc795e3a5e1263fdbbb1d1a0fc2
evnwttn <evnwttn@gmail.com>
2022-08-27 13:41:19 -0230

    prints session data to file

--- myapp/src/handlers/sessionData.ts
+++ myapp/src/handlers/sessionData.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express';
+import * as fs from 'fs/promises';
 
 export async function sessionDataHandler(req: Request, res: Response) {
-  console.log('yo')
+    console.log(JSON.stringify(req.body, null, 4));
+    try {
+      await fs.writeFile('test.json', req.body);
+      res
+        .status(200)
+        .json({ message: `earf is flat` });
+    } catch (error) {
+      res.sendStatus(500)
+    }
+
 }
