6bc735b2287f40bfa068ff29559f2d7d8677302e
evnwttn <evnwttn@gmail.com>
2022-08-29 20:41:00 -0230

    Update sessionData.ts

--- myapp/src/handlers/sessionData.ts
+++ myapp/src/handlers/sessionData.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import * as fs from 'fs/promises';
 
 export async function sessionDataHandler(req: Request, res: Response) {
-    console.log(JSON.stringify(req.body.author, null, 4));
     try {
       await fs.writeFile('test.json', JSON.stringify(req.body, null, 4));
       res
