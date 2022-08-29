82ecc3aad679aa1eeea49f28f21ded4ccfcd29a9
evnwttn <evnwttn@gmail.com>
2022-08-29 20:47:17 -0230

    formatting

--- myapp/src/handlers/new.ts
+++ myapp/src/handlers/new.ts
@@ -3,10 +3,10 @@ import * as fs from 'fs/promises';
 
 export async function newSessionHandler(req: Request, res: Response) {
     try {
-      await fs.writeFile('test.json', JSON.stringify(req.body, null, 4));
+      await fs.writeFile('test.json', JSON.stringify(req.body, null, 1));
       res
         .status(200)
-        .json({ message: `session from ${req.body.author} recieved` });
+        .json({ message: `${req.body.author} session recieved` });
     } catch (error) {
       res.sendStatus(500)
     }
