02a6f34ea53e3fbf151a18f88038f68611071a95
evnwttn <evnwttn@gmail.com>
2022-09-13 20:15:50 -0230

    testing spacing

--- myapp/src/services/database/FileAppend.ts
+++ myapp/src/services/database/FileAppend.ts
@@ -1,10 +1,11 @@
 import { Database } from '.';
 import { Session } from '../../types';
 import * as fs from "fs/promises";
+import os from 'os';
 
 export class FileAppend extends Database {
     async modifySession(session: Omit<Session, 'id'>): Promise<void> { 
-        await fs.appendFile('sessions.json', JSON.stringify(session));
+        await fs.appendFile('sessions.json', JSON.stringify(session) + os.EOL);
     }
 }
 
