94fad2302fd33140f50e51b1399ce6053fa3a798
evnwttn <evnwttn@gmail.com>
2022-09-13 19:49:21 -0230

    Update FileAppend.ts

--- myapp/src/services/FileAppend.ts
+++ myapp/src/services/FileAppend.ts
@@ -4,6 +4,7 @@ import * as fs from "fs/promises";
 
 export class FileAppend extends Database {
     async createNewSession(session: Omit<Session, 'id'>): Promise<void> { 
-        await fs.writeFile('sessions.json', JSON.stringify(session));
+        await fs.appendFile('sessions.json', JSON.stringify(session));
     }
 }
+
