8ded2402c510aec00bcb2496fbd8392d4d57bd1d
evnwttn <evnwttn@gmail.com>
2022-09-14 20:31:36 -0230

    updating data recieved types

--- myapp/src/types/Session.ts
+++ myapp/src/types/Session.ts
@@ -2,6 +2,7 @@ import { Track } from "./Track";
 
 export interface Session {
     id: string;
+    sessionTitle: string;
     author: string;
     tracks: Track[];
     parameters: string[];
--- myapp/src/types/TrackParams.ts
+++ myapp/src/types/TrackParams.ts
@@ -2,6 +2,5 @@ type HexCode = `#${string}`
 
 export interface TrackParams {
     parameter: string;
-    colour: HexCode;
     comment: string;
 }
