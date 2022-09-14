8b58a2abb827b97ff1b9820b180ba422980e90ec
evnwttn <evnwttn@gmail.com>
2022-09-14 20:33:48 -0230

    Update update.ts

--- myapp/src/handlers/update.ts
+++ myapp/src/handlers/update.ts
@@ -7,10 +7,9 @@ export async function updateSessionHandler(req: Request, res: Response) {
       const db = new FileAppend();
       await db.modifySession(req.body as Session);
       res
-        .status(200)
-        .json({ message: `${req.body.author} session recieved` });
+        .status(StatusCodes.OK)
     } catch (error) {
-      res.sendStatus(500)
+      res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
     }
 
 }
