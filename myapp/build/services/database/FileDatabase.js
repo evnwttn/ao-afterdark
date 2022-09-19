f9699592a4420d4fbc54196f93bbbc51139064da
evnwttn <evnwttn@gmail.com>
2022-09-19 14:15:11 -0230

    filedb constructor

--- myapp/build/services/database/FileDatabase.js
+++ myapp/build/services/database/FileDatabase.js
@@ -22,32 +22,27 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.FileDatabase = void 0;
 const _1 = require(".");
 const uuid_1 = require("uuid");
 const fs = __importStar(require("fs/promises"));
 const os = __importStar(require("os"));
-const sessionId = { id: (0, uuid_1.v4)() };
 class FileDatabase extends _1.Database {
-    createSession(session) {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield fs.appendFile('sessions.json', JSON.stringify(session) + os.EOL);
-        });
-    }
-    updateSession(session) {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield fs.writeFile('sessions.json', JSON.stringify(session) + os.EOL);
-        });
-    }
 }
 exports.FileDatabase = FileDatabase;
+constructor;
+{
+    const sessionId = { id: (0, uuid_1.v4)() };
+}
+async;
+createSession(session, Session);
+Promise < void  > {
+    await: fs.appendFile('sessions.json', JSON.stringify(session) + os.EOL)
+};
+async;
+updateSession(session, Session);
+Promise < void  > {
+    await: fs.writeFile('sessions.json', JSON.stringify(session) + os.EOL)
+};
+;
--- myapp/src/services/database/FileDatabase.ts
+++ myapp/src/services/database/FileDatabase.ts
@@ -1,12 +1,15 @@
 import { Database } from '.';
-import { Session } from '../../types';
+import { Session, SessionId } from '../../types';
 import { v4 as uuidv4 } from 'uuid';
 import * as fs from "fs/promises";
 import * as os from 'os';
 
-const sessionId = {id: uuidv4()};
-
 export class FileDatabase extends Database {
+
+    constructor {
+        const sessionId = {id: uuidv4()};
+    }
+
     async createSession(session: Session): Promise<void> { 
         await fs.appendFile('sessions.json', JSON.stringify(session) + os.EOL);
     }
