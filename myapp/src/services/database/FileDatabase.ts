39b1af3ad74879964e877541e5eb7e7d015b467b
evnwttn <evnwttn@gmail.com>
2022-09-20 19:21:07 -0230

    x

--- myapp/src/services/database/FileDatabase.ts
+++ myapp/src/services/database/FileDatabase.ts
@@ -2,9 +2,12 @@ import { Database } from '.';
 import { Session, SessionId } from '../../types';
 import * as fs from "fs/promises";
 import * as os from 'os';
+import { v4 as uuidv4 } from 'uuid';
+
 
 export class FileDatabase extends Database {
     async createSession(session: Session): Promise<void> { 
+        await session.id.push(uuidv4());
         await fs.appendFile('sessions.json', JSON.stringify(session) + os.EOL);
     }
 
--- myapp/src/types/Session.ts
+++ myapp/src/types/Session.ts
@@ -5,6 +5,6 @@ export interface Session {
     author: string;
     tracks: Track[];
     parameters: string[];
-    id: string;
+    id: any;
 }
 
