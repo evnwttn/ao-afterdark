99bfd8a3d62aae377e3a39ece6bca8aa898e3ca9
evnwttn <evnwttn@gmail.com>
2022-09-21 19:51:02 -0230

    Update FileDatabase.ts

--- myapp/src/services/database/FileDatabase.ts
+++ myapp/src/services/database/FileDatabase.ts
@@ -4,15 +4,13 @@ import * as fs from "fs/promises";
 import * as os from 'os';
 import { v4 as uuidv4 } from 'uuid';
 
-
-
 export class FileDatabase extends Database {
     async createSession(session: Session): Promise<void> { 
-        await fs.appendFile('sessions.json', JSON.stringify(session, null, 2) + os.EOL);
+        await fs.appendFile('sessions.json', JSON.stringify(session) + os.EOL);
     }
 
     async updateSession(session: Session): Promise<void> {
-        await fs.writeFile('sessions.json', JSON.stringify(session, null, 2) + os.EOL)
+        await fs.writeFile('sessions.json', JSON.stringify(session) + os.EOL)
     }
  }
 
