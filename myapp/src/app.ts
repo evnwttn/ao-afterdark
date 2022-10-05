c9a70bef327d8095959152811574db894a5b3626
evnwttn <evnwttn@gmail.com>
2022-10-05 20:14:22 -0230

    Update app.ts

--- myapp/src/app.ts
+++ myapp/src/app.ts
@@ -17,6 +17,3 @@ app.put("/session/", sessionHandler);
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
-
-// change validation to under 14 for length of track/para titles\
-// must be altered in react as well
