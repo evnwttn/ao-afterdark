a9ecdfb378863eac626960a92b5ce71f226135b4
evnwttn <evnwttn@gmail.com>
2022-10-27 20:45:00 -0230

    set exp date function

--- myapp/src/handlers/cookieHandler.ts
+++ myapp/src/handlers/cookieHandler.ts
@@ -1,1 +1,8 @@
+function setExpirationDate(): string {
+  let date = new Date();
+  date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000);
+
+  return date.toUTCString();
+}
+
 export async function cookieHandler() {}
