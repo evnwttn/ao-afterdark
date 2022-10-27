b926f8571d3fba22bee4d5fcda15461099844309
evnwttn <evnwttn@gmail.com>
2022-10-27 20:45:19 -0230

    imports

--- myapp/src/handlers/cookieHandler.ts
+++ myapp/src/handlers/cookieHandler.ts
@@ -1,3 +1,6 @@
+import { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+
 function setExpirationDate(): string {
   let date = new Date();
   date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000);
