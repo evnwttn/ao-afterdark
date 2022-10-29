6b29b2f0386564293c70005d10e0e8c24c66daf8
evnwttn <evnwttn@gmail.com>
2022-10-29 17:01:20 -0230

    30 days

--- myapp/build/middleware/sessionOptions.js
+++ myapp/build/middleware/sessionOptions.js
@@ -2,20 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sessionOptions = void 0;
 const uuid_1 = require("uuid");
-function setExpirationDate() {
-    let date = new Date();
-    date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000);
-    return date;
-}
 const secret = (0, uuid_1.v4)();
-const maxAge = setExpirationDate();
 exports.sessionOptions = {
     secret: secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        maxAge: maxAge,
         httpOnly: true,
+        maxAge: 2592000000,
     },
 };
 //# sourceMappingURL=sessionOptions.js.map
--- myapp/src/middleware/sessionOptions.ts
+++ myapp/src/middleware/sessionOptions.ts
@@ -1,21 +1,13 @@
 import { v4 as uuidv4 } from "uuid";
 
-function setExpirationDate(): any {
-  let date = new Date();
-  date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000);
-
-  return date;
-}
-
 const secret = uuidv4();
-const maxAge = setExpirationDate();
 
 export const sessionOptions = {
   secret: secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
-    maxAge: maxAge,
     httpOnly: true,
+    maxAge: 2592000000,
   },
 };
