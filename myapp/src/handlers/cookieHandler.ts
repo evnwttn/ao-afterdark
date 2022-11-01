a17ff8d9a8aa8adee08b468ecebb41cef706a199
evnwttn <evnwttn@gmail.com>
2022-11-01 21:25:06 -0230

    cookie HANDLA

---
+++ myapp/src/handlers/cookieHandler.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+import { StatusCodes, UserLoginData } from "../types";
+
+// NEED TO REMOVE ALL INFO FROM RETURNS THAT IS NOT SESSION ID!!!!!!
+
+export async function userHandler(req: Request, res: Response) {}
