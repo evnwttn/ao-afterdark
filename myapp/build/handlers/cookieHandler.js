c3b728375a13a9023387fc80eeca7be4b0221150
evnwttn <evnwttn@gmail.com>
2022-11-09 21:09:50 -0330

    1123

--- myapp/build/handlers/cookieHandler.js
+++ myapp/build/handlers/cookieHandler.js
@@ -16,8 +16,7 @@ function cookieHandler(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const db = new database_1.FileDatabase();
-            console.log(req.cookies);
-            console.log(req.signedCookies);
+            console.log(req.session);
             const retrieveUser = yield db.retrieveUser(req.body);
             res.status(types_1.StatusCodes.OK).json(retrieveUser);
         }
--- myapp/src/handlers/cookieHandler.ts
+++ myapp/src/handlers/cookieHandler.ts
@@ -6,8 +6,7 @@ export async function cookieHandler(req: Request, res: Response) {
   try {
     const db = new FileDatabase();
 
-    console.log(req.cookies);
-    console.log(req.signedCookies);
+    console.log(req.session);
 
     const retrieveUser = await db.retrieveUser(req.body as object);
 
