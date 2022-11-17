c1aa9d4bfa67577fef960de36ecf80eb45306ade
evnwttn <evnwttn@gmail.com>
2022-11-17 16:07:36 -0330

    Update loadGridHandler.ts

--- myapp/src/handlers/loadGridHandler.ts
+++ myapp/src/handlers/loadGridHandler.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
-import { FileDatabase } from "../services/database";
 import { Grid, StatusCodes } from "../types";
+import { db } from "../services/database";
 
 export async function loadGridHandler(req: Request, res: Response) {
   try {
-    const db = new FileDatabase();
+    await db.retrieveGrids(req.session.userId as string);
 
-    const retrievedGrids = await db.retrieveGrids(req.session.userId as string);
+    // const retrievedGrids = await db.retrieveGrids(req.session.userId as string);
 
-    res.status(StatusCodes.OK).json(retrievedGrids as Grid[]);
+    // res.status(StatusCodes.OK).json(retrievedGrids as Grid[]);
   } catch (error) {
     res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
   }
