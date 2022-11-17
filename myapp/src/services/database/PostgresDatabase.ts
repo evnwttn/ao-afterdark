acaea8af1d0e8c89e35f3a280d51a41f89bf5dbd
evnwttn <evnwttn@gmail.com>
2022-11-17 13:24:13 -0330

    :fire:

--- myapp/src/services/database/PostgresDatabase.ts
+++ myapp/src/services/database/PostgresDatabase.ts
@@ -4,18 +4,32 @@ import { UserLoginData, Grid } from "../../types";
 
 export class PostgresDatabase extends Database {
   sql: any;
+  host?: string;
+  database?: string;
+  user?: string;
+  password?: string;
 
-  // constructor
-  // this.host...
-  // use .env
+  constructor(postgresOptions: {
+    host?: string;
+    database?: string;
+    user?: string;
+    password?: string;
+  }) {
+    super();
+
+    this.host = postgresOptions.host;
+    this.database = postgresOptions.database;
+    this.user = postgresOptions.user;
+    this.password = postgresOptions.password;
+  }
 
   connect(): Promise<void> {
     this.sql = postgres({
-      host: "xxxxxxx",
-      database: "xxxxx",
-      port: 42069,
-      user: "xxxxxxxx",
-      password: "xxxxxxxxxxxxxxxxxx",
+      port: 5432,
+      host: this.host,
+      database: this.database,
+      user: this.user,
+      password: this.password,
     });
 
     return Promise.resolve();
--- myapp/src/services/database/index.ts
+++ myapp/src/services/database/index.ts
@@ -4,8 +4,13 @@ export * from "./Database";
 export * from "./FileDatabase";
 export * from "./PostgresDatabase";
 
-const _db = new PostgresDatabase();
+export const postgresOptions = {
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  user: process.env.PG_USER,
+  password: process.env.PG_PW,
+};
+
+const _db = new PostgresDatabase(postgresOptions);
 _db.connect();
 export const db = _db;
-
-// process.env.....
