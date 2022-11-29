c23e960a4ac52b980016e5f875987d8d6553ab72
evnwttn <evnwttn@gmail.com>
2022-11-29 20:20:14 -0330

    Update index.ts

--- myapp/src/types/index.ts
+++ myapp/src/types/index.ts
@@ -4,8 +4,6 @@ export * from "./TrackParams";
 export * from "./StatusCodes";
 export * from "./UserLoginData";
 
-import { Session } from "express-session";
-
 declare module "express-session" {
   interface Session {
     userId: string;
