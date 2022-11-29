628ac6c1c4138d82a05a4830fe269bd3a7c84a8f
evnwttn <evnwttn@gmail.com>
2022-11-28 21:29:03 -0330

    Update Grid.ts

--- myapp/src/types/Grid.ts
+++ myapp/src/types/Grid.ts
@@ -1,10 +1,10 @@
 import { Track } from "./Track";
 
 export interface Grid {
-  sessionTitle: string;
-  author: string;
-  tracks: Track[];
   parameters: string[];
-  grid_id: string;
   user_id: string;
+  author: string;
+  session_title: string;
+  tracks: Track[];
+  grid_id: string;
 }
