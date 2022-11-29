604ab803baab215629423205202c8506683b4674
evnwttn <evnwttn@gmail.com>
2022-11-29 17:49:09 -0330

    Revert "redundant import"
    This reverts commit 6155b9b50b048aaac768bbe7746637c7bc165292.

--- myapp/src/types/index.ts
+++ myapp/src/types/index.ts
@@ -4,6 +4,8 @@ export * from "./TrackParams";
 export * from "./StatusCodes";
 export * from "./UserLoginData";
 
+import { Session } from "express-session";
+
 declare module "express-session" {
   interface Session {
     userId: string;
