b0bccfc3f530c139d9c1bf553ce149b16fec5e08
evnwttn <evnwttn@gmail.com>
2022-12-07 21:56:43 -0330

    :zzz:

--- myapp/build/middleware/cors.js
+++ myapp/build/middleware/cors.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsOptions = void 0;
 exports.corsOptions = {
     methods: "POST, PUT, GET, OPTIONS",
-    allowedHeaders: "Access-Control-Allow-Headers, Origin, Authorization, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+    allowedHeaders: "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Authorization, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
     credentials: true,
     preflightContinue: true,
 };
--- myapp/src/middleware/cors.ts
+++ myapp/src/middleware/cors.ts
@@ -1,7 +1,7 @@
 export const corsOptions = {
   methods: "POST, PUT, GET, OPTIONS",
   allowedHeaders:
-    "Access-Control-Allow-Headers, Origin, Authorization, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+    "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Authorization, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
   credentials: true,
   preflightContinue: true,
 };
