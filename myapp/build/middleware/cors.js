a182ff3690e56832086a775900e198c23d8812a8
evnwttn <evnwttn@gmail.com>
2022-12-11 21:16:56 -0330

    :clock2:

--- myapp/build/middleware/cors.js
+++ myapp/build/middleware/cors.js
@@ -4,9 +4,9 @@ exports.corsHandler = void 0;
 function corsHandler(req, res, next) {
     res.set({
         "Access-Control-Allow-Origin": "http://localhost:3000",
-        "Access-Control-Allow-Methods": "GET,PUT,POST",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,OPTIONS",
         "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Headers": "Accept, Content-Type, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, User-Agent",
     });
     next();
 }
--- myapp/src/middleware/cors.ts
+++ myapp/src/middleware/cors.ts
@@ -3,9 +3,10 @@ import { Request, Response, NextFunction } from "express";
 export function corsHandler(req: Request, res: Response, next: NextFunction) {
   res.set({
     "Access-Control-Allow-Origin": "http://localhost:3000",
-    "Access-Control-Allow-Methods": "GET,PUT,POST",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,OPTIONS",
     "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Headers":
+      "Accept, Content-Type, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, User-Agent",
   });
 
   next();
