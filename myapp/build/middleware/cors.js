98069aa155132cb7325479fffaf7f1cabadab0e7
evnwttn <evnwttn@gmail.com>
2022-12-11 21:55:51 -0330

    :clock3:

--- myapp/build/middleware/cors.js
+++ myapp/build/middleware/cors.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsHandler = void 0;
 function corsHandler(req, res, next) {
     res.set({
-        "Access-Control-Allow-Origin": "http://localhost:3000",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,OPTIONS",
+        "Access-Control-Allow-Origin": "http://localhost:3000'",
+        "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS, POST, PUT",
         "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Headers": "Accept, Content-Type, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, User-Agent",
+        "Access-Control-Allow-Headers": "Accept, Content-Type, Referer, User-Agent",
     });
     next();
 }
--- myapp/src/middleware/cors.ts
+++ myapp/src/middleware/cors.ts
@@ -2,11 +2,10 @@ import { Request, Response, NextFunction } from "express";
 
 export function corsHandler(req: Request, res: Response, next: NextFunction) {
   res.set({
-    "Access-Control-Allow-Origin": "http://localhost:3000",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,OPTIONS",
+    "Access-Control-Allow-Origin": "http://localhost:3000'",
+    "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS, POST, PUT",
     "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Headers":
-      "Accept, Content-Type, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, User-Agent",
+    "Access-Control-Allow-Headers": "Accept, Content-Type, Referer, User-Agent",
   });
 
   next();
