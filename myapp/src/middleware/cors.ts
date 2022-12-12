31df67f0c7739a12c70f85cdb98136c5817dfbd4
evnwttn <evnwttn@gmail.com>
2022-12-11 22:04:47 -0330

    :clock4:

--- myapp/src/middleware/cors.ts
+++ myapp/src/middleware/cors.ts
@@ -2,10 +2,10 @@ import { Request, Response, NextFunction } from "express";
 
 export function corsHandler(req: Request, res: Response, next: NextFunction) {
   res.set({
-    "Access-Control-Allow-Origin": "http://localhost:3000'",
-    "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS, POST, PUT",
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Methods": "POST, PUT, OPTIONS",
     "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Headers": "Accept, Content-Type, Referer, User-Agent",
+    "Access-Control-Allow-Headers": "Content-Type",
   });
 
   next();
