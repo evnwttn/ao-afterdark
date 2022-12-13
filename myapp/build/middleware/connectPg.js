8b9c02c109808acc52cf30c5c99676abddd85202
evnwttn <evnwttn@gmail.com>
2022-12-13 20:24:54 -0330

    :hammer: :clock3:

---
+++ myapp/build/middleware/connectPg.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.connectPg = void 0;
+const express_session_1 = __importDefault(require("express-session"));
+const connect_pg_simple_1 = __importDefault(require("connect-pg-simple"));
+const SessionStore = (0, connect_pg_simple_1.default)(express_session_1.default);
+const storeOptions = {};
+exports.connectPg = {
+    store: new SessionStore(storeOptions),
+    secret: "shhh",
+    retries: 0,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 2592000000,
+        secure: false,
+    },
+};
+//# sourceMappingURL=connectPg.js.map
---
+++ myapp/build/services/database/index.js
@@ -0,0 +1,60 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+var _a, _b, _c, _d;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.db = exports.postgresOptions = void 0;
+const PostgresDatabase_1 = require("./PostgresDatabase");
+__exportStar(require("./Database"), exports);
+__exportStar(require("./PostgresDatabase"), exports);
+// export const postgresOptions: PostgresOptions = {
+//   port: 7142,
+//   host: process.env.PG_HOST ?? "",
+//   database: process.env.PG_DB ?? "",
+//   user: process.env.PG_USER ?? "",
+//   password: process.env.PG_PW ?? "",
+// };
+exports.postgresOptions = {
+    port: 7142,
+    host: (_a = process.env.PGHOST) !== null && _a !== void 0 ? _a : "",
+    database: (_b = process.env.PGDATABASE) !== null && _b !== void 0 ? _b : "",
+    user: (_c = process.env.PGUSER) !== null && _c !== void 0 ? _c : "",
+    password: (_d = process.env.PGPASSWORD) !== null && _d !== void 0 ? _d : "",
+};
+const _db = new PostgresDatabase_1.PostgresDatabase(exports.postgresOptions);
+_db.connect();
+exports.db = _db;
+// const main = async () => {
+//   _db.updateGridTracks({
+//     author: "123",
+//     id: "234",
+//     parameters: ["title", "11111111111"],
+//     sessionTitle: "123",
+//     tracks: [
+//       {
+//         title: "11111111111",
+//         parameters: [
+//           { parameter: "11111111111", colour: "#7284A8", comment: "" },
+//         ],
+//       },
+//     ],
+//   });
+// };
+// main().then(() => console.log("sent"));
+// read in config.son
+// check what database to use -> database: { postgres: .. } || database: { file: }
+// parse the 'database' config and then build whichever depending on config
+// const _db = config.database.postgres ? new PostgresDatabase(config.database) : new File
+//# sourceMappingURL=index.js.map
