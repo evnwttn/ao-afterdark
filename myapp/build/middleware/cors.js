8a499e453602d6875880ceaf41ba732ea8a78ec3
evnwttn <evnwttn@gmail.com>
2022-12-12 20:34:52 -0330

    rejig to deployed frontend

--- myapp/build/middleware/cors.js
+++ myapp/build/middleware/cors.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsHandler = void 0;
 function corsHandler(req, res, next) {
     res.set({
-        "Access-Control-Allow-Origin": "http://localhost:3000",
+        "Access-Control-Allow-Origin": "https://evnwttn.github.io/ao/",
         "Access-Control-Allow-Methods": "POST, PUT, OPTIONS",
         "Access-Control-Allow-Credentials": "true",
         "Access-Control-Allow-Headers": "Content-Type",
--- myapp/src/middleware/cors.ts
+++ myapp/src/middleware/cors.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 export function corsHandler(req: Request, res: Response, next: NextFunction) {
   res.set({
-    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Origin": "https://evnwttn.github.io/ao/",
     "Access-Control-Allow-Methods": "POST, PUT, OPTIONS",
     "Access-Control-Allow-Credentials": "true",
     "Access-Control-Allow-Headers": "Content-Type",
