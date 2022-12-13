fe86383e35bd0ccb87d6fc8f9420b97cdb51a8ba
evnwttn <evnwttn@gmail.com>
2022-12-12 20:43:55 -0330

    yo

--- myapp/build/middleware/cors.js
+++ myapp/build/middleware/cors.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsHandler = void 0;
 function corsHandler(req, res, next) {
     res.set({
-        "Access-Control-Allow-Origin": "https://evnwttn.github.io/ao/",
-        "Access-Control-Allow-Methods": "POST, PUT, OPTIONS",
+        "Access-Control-Allow-Origin": "https://evnwttn.github.io",
+        "Access-Control-Allow-Methods": "POST, PUT, GET, OPTIONS",
         "Access-Control-Allow-Credentials": "true",
         "Access-Control-Allow-Headers": "Content-Type",
     });
--- myapp/src/middleware/cors.ts
+++ myapp/src/middleware/cors.ts
@@ -2,8 +2,8 @@ import { Request, Response, NextFunction } from "express";
 
 export function corsHandler(req: Request, res: Response, next: NextFunction) {
   res.set({
-    "Access-Control-Allow-Origin": "https://evnwttn.github.io/ao/",
-    "Access-Control-Allow-Methods": "POST, PUT, OPTIONS",
+    "Access-Control-Allow-Origin": "https://evnwttn.github.io",
+    "Access-Control-Allow-Methods": "POST, PUT, GET, OPTIONS",
     "Access-Control-Allow-Credentials": "true",
     "Access-Control-Allow-Headers": "Content-Type",
   });
