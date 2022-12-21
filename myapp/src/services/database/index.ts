408e23a7b156691727a62e2cccc576cbf65af49f
evnwttn <evnwttn@gmail.com>
2022-12-21 19:56:45 -0330

    Update index.ts

--- myapp/src/services/database/index.ts
+++ myapp/src/services/database/index.ts
@@ -4,14 +4,6 @@ import { PostgresOptions } from "../../types";
 export * from "./Database";
 export * from "./PostgresDatabase";
 
-// export const postgresOptions: PostgresOptions = {
-//   port: 7142,
-//   host: process.env.PG_HOST ?? "",
-//   database: process.env.PG_DB ?? "",
-//   user: process.env.PG_USER ?? "",
-//   password: process.env.PG_PW ?? "",
-// };
-
 export const postgresOptions: PostgresOptions = {
   port: 7142,
   host: process.env.PGHOST ?? "",
@@ -23,25 +15,6 @@ export const postgresOptions: PostgresOptions = {
 const _db = new PostgresDatabase(postgresOptions);
 _db.connect();
 export const db = _db;
-
-// const main = async () => {
-//   _db.updateGridTracks({
-//     author: "123",
-//     id: "234",
-//     parameters: ["title", "11111111111"],
-//     sessionTitle: "123",
-//     tracks: [
-//       {
-//         title: "11111111111",
-//         parameters: [
-//           { parameter: "11111111111", colour: "#7284A8", comment: "" },
-//         ],
-//       },
-//     ],
-//   });
-// };
-
-// main().then(() => console.log("sent"));
 
 // read in config.son
 // check what database to use -> database: { postgres: .. } || database: { file: }
